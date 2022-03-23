25462c4d8fce34f115a2c4df30dea3bb2495f0e3
kotaegwon <rhxornjs@naver.com>
2022-03-23 20:59:45 +0900

    일기장

--- app/src/main/java/org/techtown/mydiary/Fragment2.java
+++ app/src/main/java/org/techtown/mydiary/Fragment2.java
@@ -1,15 +1,41 @@
 package org.techtown.mydiary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
+
+    Context context;
+    OnTabItemSelectedListener listener;
+    Button save_btn,del_btn,close_btn;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context=context;
+
+        if(context instanceof OnTabItemSelectedListener){
+            listener=(OnTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        if(context!=null){
+            context=null;
+            listener=null;
+        }
+    }
 
     @Nullable
     @Override
@@ -18,6 +44,25 @@ public class Fragment2 extends Fragment {
         initUI(rootview);
         return rootview;
     }
+
+    //버튼 눌렀을때 화면 이동
     private void initUI(ViewGroup rootview) {
+        save_btn=rootview.findViewById(R.id.saveButton);
+        del_btn=rootview.findViewById(R.id.deleteButton);
+        close_btn=rootview.findViewById(R.id.closeButton);
+
+        View.OnClickListener clickListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.saveButton:
+                        break;
+                    case R.id.deleteButton:
+                        break;
+                    case R.id.closeButton:
+                        break;
+                }
+            }
+        };
     }
 }
